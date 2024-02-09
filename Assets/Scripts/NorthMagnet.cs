aa5399c3f8ed4dd558a6c12430760e705209c10b
jmanoj0905 <148736462+jmanoj0905@users.noreply.github.com>
2024-02-09 18:21:16 +0530

    Magnets v2

--- Assets/Scripts/NorthMagnet.cs
+++ Assets/Scripts/NorthMagnet.cs
@@ -1,43 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NorthMagnet : MonoBehaviour
 {
     public LayerMask southPole;
     public int numberOfRaycasts = 90;
-    // Start is called before the first frame update
-    void Start()
+    private float magneticForce = 0.2f;
+    public Rigidbody2D rb;
+    public float raycastDelay = 0.05f; // Adjust the delay between each set of raycasts
+
+    private void Start()
     {
-        
+        StartCoroutine(CastRays());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CastRays()
     {
-        IsOppMagNearBy(); //Gives true or false based on if a magnet is nearby
-        if(IsOppMagNearBy())
+        while (true)
         {
-                for (int i = 0; i < numberOfRaycasts; i++)
+            for (int i = 0; i < numberOfRaycasts; i++)
             {
                 float angle = i * (360f / numberOfRaycasts);
                 Vector2 rayDirection = Quaternion.Euler(0, 0, angle) * Vector2.right;
-
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, 10, southPole);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, Mathf.Infinity, southPole);
                 Debug.DrawRay(transform.position, rayDirection * 10, Color.green); // Draw the ray in the Scene view
-
                 // Check if the ray hit something
-                if (hit.collider != null)
+                if (hit.collider != null && hit.collider.CompareTag("SouthPole"))
                 {
-                    Debug.Log("Hit something at angle: " + angle);
+                    Vector2 southPolePos = hit.collider.gameObject.transform.position;
+                    Attract(southPolePos);
                     // Do something with the hit object
                 }
             }
+            yield return new WaitForSeconds(raycastDelay); // Wait for the specified delay before casting the next set of rays
         }
-        
     }
-    public bool IsOppMagNearBy()
+
+    public void Attract(Vector2 southPolePos)
     {
-        return Physics2D.OverlapCircle(transform.position , 10f, southPole);
+        Vector2 attractDirection = southPolePos - (Vector2)transform.position;
+        rb.AddForce(attractDirection.normalized * magneticForce, ForceMode2D.Force);
     }
 }
